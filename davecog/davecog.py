cc9b00187774b935916d056c8887b14bc2009f19
TalkingCactus <talkingcactus98@gmail.com>
2017-02-22 16:56:02 -0500

    maybe this will work

--- davecog/davecog.py
+++ davecog/davecog.py
@@ -1,20 +1,13 @@
 import discord
 from discord.ext import commands
 
-class DaveCog:
+class autoupdate:
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-
-    async def autoupdate(bot)
-        update()
-
-    async def mycom(self):
-        """This does stuff!"""
-
-        #Your code will go here
-        await self.bot.say("I can do stuff!")
+    async def autoupdate(self):
+        bot.update()
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
